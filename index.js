85df1bc36bd55003854b45e0d3804804262c1d45
Sree Charitha <38373549+RSreeCharitha@users.noreply.github.com>
2020-01-07 17:10:31 +0530

    js if

--- index.js
+++ index.js
@@ -9,13 +9,36 @@ document.getElementById("clear").addEventListener("click", function(){
 
 
 function compute() {
+  
   var bin = document.getElementsByClassName("bin")[0].value;
   var hex = document.getElementsByClassName("hex")[0].value;
   var oct = document.getElementsByClassName("oct")[0].value;
   var dec = document.getElementsByClassName("dec")[0].value;
 
-  document.getElementsByClassName("oct")[0].value = parseInt(dec,10).toString(8);
+  if(dec != ""){
+    document.getElementsByClassName("oct")[0].value = parseInt(dec,10).toString(8);
   document.getElementsByClassName("hex")[0].value = parseInt(dec,10).toString(16);
   document.getElementsByClassName("bin")[0].value = parseInt(dec,10).toString(2);
+  }
+   if (bin!="") {
+    document.getElementsByClassName("oct")[0].value = parseInt(bin,2).toString(8);
+    document.getElementsByClassName("hex")[0].value = parseInt(bin,2).toString(16);
+    document.getElementsByClassName("dec")[0].value = parseInt(bin,2).toString(10);
+  }
+   if(hex!=""){
+    document.getElementsByClassName("oct")[0].value = parseInt(hex,16).toString(8);
+    document.getElementsByClassName("bin")[0].value = parseInt(hex,16).toString(2);
+    document.getElementsByClassName("dec")[0].value = parseInt(hex,16).toString(10);
+
+  }
+
+   if(oct!=""){
+    document.getElementsByClassName("bin")[0].value = parseInt(oct,8).toString(2);
+    document.getElementsByClassName("hex")[0].value = parseInt(oct,8).toString(16);
+    document.getElementsByClassName("dec")[0].value = parseInt(oct,8).toString(10);
+
+  }
+
+  
 
 }
